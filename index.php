4940a80c0ade4bafb6a0458ffb94a78aaf70852d
John Gibbons <johngibbons10@gmail.com>
2014-12-17 20:48:49 -0700

    blog page and posts styling

--- index.php
+++ index.php
@@ -6,15 +6,25 @@
 
 						<main id="main" class="cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
+							<h1 class="page-title" itemprop="headline"><?php echo get_the_title( get_option( 'page_for_posts' ) ); ?></h1>
+
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
 
 								<header class="article-header">
+									<div class="single-hero-container">
+										<?php if ( has_post_thumbnail() ) : ?>
+											<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+												<?php the_post_thumbnail('large', array('class' => 'single-hero')); ?>
+											</a>
+										<?php endif; ?>
+
+                  </div>
 
 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 									<p class="byline entry-meta vcard">
-                                        				<?php printf( __( 'Posted %1$s by %2$s', 'bonestheme' ),
+                                        				<?php printf( __( '%1$s', 'bonestheme' ),
                        								/* the time the post was published */
                        								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
                        								/* the author of the post */
@@ -25,21 +35,8 @@
 								</header>
 
 								<section class="entry-content cf">
-									<?php the_content(); ?>
+									<?php the_excerpt(); ?>
 								</section>
-
-								<footer class="article-footer cf">
-									<p class="footer-comment-count">
-										<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
-									</p>
-
-
-                 	<?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
-
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-
-								</footer>
 
 							</article>
 
@@ -65,8 +62,6 @@
 
 
 						</main>
-
-					<?php get_sidebar(); ?>
 
 				</div>
 
